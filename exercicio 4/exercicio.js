086f484bf0c706143da94b858d29a1f516e7ca2a
Alexandre Bittencourt Bassani <166077081+AlexandreBassani@users.noreply.github.com>
2024-06-24 23:16:11 -0300

    Add files via upload

---
+++ exercicio 4/exercicio.js
@@ -0,0 +1,20 @@
+// Cria uma lista vazia
+let listaGenerica = [];
+
+// Cria uma lista de linguagens de programação
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin'];
+
+// Adiciona mais elementos à lista de linguagens de programação
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+// Cria uma lista com 3 nomes
+let nomes = ['João', 'Maria', 'Pedro'];
+
+// Exibe no console o primeiro elemento da lista de nomes
+console.log('Primeiro elemento da lista de nomes:', nomes[0]); // Saída: Primeiro elemento da lista de nomes: João
+
+// Exibe no console o segundo elemento da lista de nomes
+console.log('Segundo elemento da lista de nomes:', nomes[1]); // Saída: Segundo elemento da lista de nomes: Maria
+
+// Exibe no console o último elemento da lista de nomes
+console.log('Último elemento da lista de nomes:', nomes[nomes.length - 1]); // Saída: Último elemento da lista de nomes: Pedro
